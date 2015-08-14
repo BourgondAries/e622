7e4d4fcf6f5afc7131b2de23e97341b3b8cd8f3c
BourgondAries <macocio@gmail.com>
2015-08-14 21:11:19 +0200

    Trim the last password character, file get contents appends a space

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
+	var_dump(function_exists('mysqli_connect'));
 	$password = file_get_contents("../mysqlkey.txt");
-	$mysqli = new mysqli("localhost", "server", $password, "e622");
-	if (mysqli_connect_errno())
+	$password = rtrim($password);
+	echo '"'.$password.'"';
+	$mysqli = new mysqli("127.0.0.1", "server", $password, "e622", "3306");
+	if ($mysqli->connect_errno)
 	{
-		printf("Connection failed: %s\n", mysqli_connect_error());
+		printf("Connection failed: %s\n", $mysqli->connect_error);
 		exit();
 	}
 
-	printf("Server version: %s\n", $mysqli->server_info);
-	$mysqli->close();
+	printf("Server version: %s\n", $mysqli->host_info);
+
 ?>
