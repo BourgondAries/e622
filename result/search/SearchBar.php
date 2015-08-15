8fa55e211c966c5c280707b4984075f02d279c20
BourgondAries <macocio@gmail.com>
2015-08-15 19:34:36 +0200

    Make the search bar nice and rounded

--- result/search/SearchBar.php
+++ result/search/SearchBar.php
@@ -4,7 +4,7 @@
 		return '<div class="searchbar">
 				<h5 id="searchblock"> Search </h5>
 				<form action="/search.php">
-					<input type="text" name="q">
+					<input id="textfield" type="text" name="q">
 					<input type="submit" value="Submit"
 						style="position: absolute; left: -100%; width: 1px; height: 1px;">
 				</form>
