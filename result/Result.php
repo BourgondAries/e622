28ed4d11c560b788d8f1483123c24fbe6d6ff167
BourgondAries <macocio@gmail.com>
2015-08-15 17:21:45 +0200

    Add a nice shadow and a surrounding box

--- result/Result.php
+++ result/Result.php
@@ -3,14 +3,16 @@
 	require_once("$root/utils/stylelink.php")
 ?>
 <div class="searchblock">
-	<div id="searchbar">
-		<?php
-			require_once 'search/SearchBar.php';
-		?>
-	</div>
-	<div id="result">
-		<?php
-			require_once 'resgen/ResultSet.php';
-		?>
+	<div id="searchcontainer">
+		<div id="searchbar">
+			<?php
+				require_once 'search/SearchBar.php';
+			?>
+		</div>
+		<div id="result">
+			<?php
+				require_once 'resgen/ResultSet.php';
+			?>
+		</div>
 	</div>
 </div>
