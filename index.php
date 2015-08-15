4c9575b5578a91e940399637c969166e177f337b
BourgondAries <macocio@gmail.com>
2015-08-15 21:35:10 +0200

    Fix middle stying and add some example elements

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 	require_once 'result/Result.php';
 	$item = array(
 		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW'),
+		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW'),
+		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW'),
+		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW'),
+		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW'),
+		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW'),
+		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW'),
 		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW')
 	);
 	$result = generateResult($item);
