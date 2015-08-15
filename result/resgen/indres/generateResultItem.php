ad816eaff9751ccb85e1ee58e8b7e15ffead0dab
BourgondAries <macocio@gmail.com>
2015-08-15 14:47:27 +0200

    Create a generator function for the search result.
    A simple function taking in basic data. It returns a string that represents
    HTML code. Next up, colors can be added to the different codes, such as SFW,
    NSFW, QSFW, as well as the 4 different numbers

--- result/resgen/indres/generateResultItem.php
+++ result/resgen/indres/generateResultItem.php
@@ -1,11 +1,25 @@
 <link rel="stylesheet" type="text/css" href="/style/searchitem.css">
-<div class="searchitem">
-	<a href=/post/something>
-		<img class="thumbnail-searchitem" alt="thumbnail" height="150" width="150" src="/db/0.jpg">
-		<span class="thumbnail-score"> Hi! </span>
-	</a>
-</div>
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
 
+	function generateResultItem($ups, $favorites, $downs, $safety)
+	{
+		$total = $ups - $downs;
+		return "<div class=\"searchitem\">
+	<a href=/post/something>
+		<img class=\"thumbnail-searchitem\" alt=\"thumbnail\" height=\"150\" width=\"150\" src=\"/db/0.jpg\">
+		<span class=\"thumbnail-score\"> $total | $ups | $favorites | $downs | $safety </span>
+	</a>
+</div>";
+	}
+
+	$amount = 50;
+	for ($iterator = 0; $iterator < $amount; ++$iterator)
+	{
+		echo generateResultItem($iterator, 0, 0, "SFW");
+	}
+?>
 <div class="searchitem">
 	<a href=/post/something>
 		<img class="thumbnail-searchitem" alt="thumbnail" height="150" width="150" src="/db/0.jpg">
