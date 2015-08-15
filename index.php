f78727944ed2531654be7dd2c9b3194095ed4099
BourgondAries <macocio@gmail.com>
2015-08-15 21:25:50 +0200

    Make the news generator an actual generator. Implement general generator.

--- index.php
+++ index.php
@@ -1,38 +1,10 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>e622 - World's Most Open Database</title>
-		<link rel="stylesheet" type="test/css" href="style/cat.css">
-	</head>
-
-	<body>
-		<div class="navbar">
-			<div id="navbar">
-				<ul>
-					<li><a href="#login">Login</a></li>
-					<li><a href="#tags">Tags</a></li>
-					<li><a href="#news">News</a></li>
-					<li><a href="#update">Update</a></li>
-				</ul>
-			</div>
-		</div>
-
-		<?php
-			$root = $_SERVER['DOCUMENT_ROOT'];
-			include "$root/utils/News.php";
-		?>
-
-		<?php
-
-		?>
-
 <?php
+	require_once 'template/Template.php';
 	require_once 'result/Result.php';
 	$item = array(
 		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW'),
 		array('id' => 1, 'up' => 1, 'down' => 3, 'fav' => 6, 'type' => 'SFW')
 	);
-	echo generateResult($item);
+	$result = generateResult($item);
+	echo generateTemplate($result);
 ?>
-	</body>
-</html>
---
+++ template/Template.php
@@ -0,0 +1,33 @@
+<?php
+	function generateTemplate($mainpage = '', $title = 'e622 - World\'s Most Open Database', $sidebar = '')
+	{
+		$root = $_SERVER['DOCUMENT_ROOT'];
+		include_once "$root/utils/News.php";
+		$news = generateNews();
+
+		return "<!DOCTYPE html>
+		<html>
+			<head>
+				<title>$title</title>
+				<link rel=\"stylesheet\" type=\"test/css\" href=\"style/cat.css\">
+			</head>
+
+			<body>
+				<div class=\"navbar\">
+					<div id=\"navbar\">
+						<ul>
+							<li><a href=\"#login\">Login</a></li>
+							<li><a href=\"#tags\">Tags</a></li>
+							<li><a href=\"#news\">News</a></li>
+							<li><a href=\"#update\">Update</a></li>
+						</ul>
+					</div>
+				</div>
+
+				$news
+
+				$mainpage
+			</body>
+		</html>";
+	}
+?>
