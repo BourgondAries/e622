a0b5df7e16db61843f28233007f3205cb177a005
BourgondAries <macocio@gmail.com>
2015-08-15 19:01:20 +0200

    Add news items

--- utils/News.php
+++ utils/News.php
@@ -1,6 +1,14 @@
-<div id="newscapsule">
-	<div id="news">
-		<strong> Sat Aug 15: </strong> The first news item is up and running! It's rather simple, but it works. There's still a tonne to do!
+<div class="news">
+	<div id="newscapsule">
+		<div id="news">
+			<ul>
+				<li>
+					<strong> Aug 15: </strong> The first news item is up and running! It's rather simple, but it works. There's still a tonne to do!
+
+				</li>
+				<li> <strong> Aug 15: </strong> We need a working database </li>
+				<li> The result page generator is done </li>
+			</ul>
+		</div>
 	</div>
 </div>
-
