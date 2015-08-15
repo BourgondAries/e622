337eb28f53ea1b0b093c4d51fd025536d19ea2ef
BourgondAries <macocio@gmail.com>
2015-08-16 00:22:26 +0200

    Add description

--- media/mediadescriber/Describer.php
+++ media/mediadescriber/Describer.php
@@ -3,7 +3,10 @@
 	{
 		$template = "<div class=\"mediadescriptor\">
 			<div id=\"media\">
-				<img alt=\"hey\" src=\"$imglink\">
+				<img alt=\"image\" src=\"$imglink\">
+			</div>
+			<div id=\"infobox\">
+				<div id=\"description\"> Description </div>
 			</div>
 		</div>";
 
