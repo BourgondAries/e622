3ae7570b0dc644b3bbb9649cc2fa5e7ba186a283
BourgondAries <macocio@gmail.com>
2015-08-17 14:54:04 +0200

    Add session liveness check

--- user/user.php
+++ user/user.php
@@ -2,6 +2,8 @@
 	$root = $_SERVER['DOCUMENT_ROOT'];
 	require_once "$root/phpass/PasswordHash.php";
 	require_once "$root/template/Template.php";
+	if (session_status() == PHP_SESSION_NONE)
+		session_start();
 	if (!isset($_GET['user']))
 	{
 		echo generateTemplate('<div class="login" id="redirectnotice"> No user specified. If you suspect to have found a bug, please report this on the support page. </div>');
