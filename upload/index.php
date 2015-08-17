cd3417a8c9663d65aa6fd0a93437184a363e096a
BourgondAries <macocio@gmail.com>
2015-08-17 04:39:12 +0200

    Fix spelling of accountable

--- upload/index.php
+++ upload/index.php
@@ -76,6 +76,6 @@
 		</div>
 	</form>";
 
-	$responsibilities = '<div class="upload"> Notice: <div class="selaboration"> e622 reserves the rights to revoke or delete any upload due to: the copyright holder\'s request of deletion, or, the breaking of any laws pertaining the media being uploaded. <br> You are responsible for the media you upload and may be held accountible. <br> You are free to upload whatever is within the bounds of the law. <br><br> The media uploaded to e622 is publicly available. By uploading you give e622 consent to propagate and redistribute copies of the media. (We need a lawyer to write this stuff). <br><br> We (e622 staff) will never delete media based on our own personal beliefs, public opinion, or whatever non-judiciary reason. </div></div>';
+	$responsibilities = '<div class="upload"> Notice: <div class="selaboration"> e622 reserves the rights to revoke or delete any upload due to: the copyright holder\'s request of deletion, or, the breaking of any laws pertaining the media being uploaded. <br> You are responsible for the media you upload and may be held accountable. <br> You are free to upload whatever is within the bounds of the law. <br><br> The media uploaded to e622 is publicly available. By uploading you give e622 consent to propagate and redistribute copies of the media. (We need a lawyer to write this stuff). <br><br> We (e622 staff) will never delete media based on our own personal beliefs, public opinion, or whatever non-judiciary reason. </div></div>';
 	echo generateTemplate($template, $responsibilities);
 ?>
