1444e183fdd36e1a9e5c115938110c27e208bc54
BourgondAries <macocio@gmail.com>
2015-08-19 18:31:42 +0200

    Add user ID fetching for mecia

--- upload/postupload.php
+++ upload/postupload.php
@@ -38,6 +38,15 @@
 		$failed = false;
 		$mysqli = $db->get();
 		$mysqli->autocommit(false);
+
+		// Find the user id first
+		$prepst = $mysqli->prepare("SELECT user_ID FROM User WHERE username=?");
+		$prepst->bind_param('s', $_SESSION['user']);
+		$res = $prepst->execute();
+		$res2 = $prepst->get_result();
+		$fetched = $res2->fetch_array(MYSQLI_NUM);
+		$userid = $fetched[0];
+
 		$res = $mysqli->query("SELECT max(media_ID) as mx FROM Media");
 		$max = $res->fetch_object();
 		$assoc_file = base_convert($max->mx + 1, 10, 36) . '.' . $extension;
@@ -45,7 +54,7 @@
 		rename($tmpname, $full_path);
 
 		$prepst = $mysqli->prepare("INSERT INTO Media (filename, description, uploader) VALUES (?, ?, ?);");
-		$prepst->bind_param('ssi', $assoc_file, $description, $num);
+		$prepst->bind_param('ssi', $assoc_file, $description, $userid);
 		$prepst->execute();
 
 		$mysqli->commit();
