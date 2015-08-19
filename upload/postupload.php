437e2fc8365ff1a22ffbb38f9506ecc5dae1858f
BourgondAries <macocio@gmail.com>
2015-08-19 07:46:47 +0200

    Commit a semi-finished uploading functionality for image uploading

---
+++ upload/postupload.php
@@ -0,0 +1,59 @@
+<?php
+	if (session_status() == PHP_SESSION_NONE)
+		session_start();
+	if (!isset($_SESSION['user']))
+	{
+		header('Location: ' . '/login/index.php?reason=upload_expired');
+		die();
+	}
+	$root = $_SERVER['DOCUMENT_ROOT'];
+	require_once "$root/utils/E621.php";
+	function makeTagIds($tag_list)
+	{
+	}
+	var_dump($_POST);
+	$tag_array = explode(' ', $_POST['tags']);
+	$description = $_POST['description'];
+	$tmpname = $_FILES['file']['tmp_name'];
+	$check = getimagesize($tmpname);
+	if ($check !== false)
+	{
+		$mime = $check['mime'];
+		$extension = '';
+		switch ($mime)
+		{
+			case 'image/png':
+				$extension = 'png';
+			break;
+		}
+		$db = new E621;
+		$db_conn = $db->get();
+
+		$failed = false;
+		$mysqli = $db->get();
+		$mysqli->autocommit(false);
+		$res = $mysqli->query("SELECT max(media_ID) FROM Media");
+		echo 'AAAAAAAAAAAAAAAAAAAAA';
+		echo($res->current_field);
+		echo 'AAAAAAAAAAAAAAAAAAAAA';
+		die();
+		$mysqli->query("INSERT INTO table VALUES ('bar')") ? NULL : $failed = true;
+		$failed ? $mysqli->rollback() : $mysqli->commit();
+
+		$db_conn_getter = $db_conn->prepare('SELECT max(media_ID) FROM Media');
+		$db_conn_getter->execute();
+		$result = $db_conn_prep->get_result();
+		$fetched = $result->fetch_array(MYSQLI_NUM);
+		var_dump($fetched);
+		die();
+
+		$db_conn_prep = $db_conn->prepare('INSERT INTO Media (description, );');
+		$db_conn_prep->bind_param('s', $username);
+		$does_exist = $db_conn_prep->execute();
+		$result = $db_conn_prep->get_result();
+		$fetched = $result->fetch_array(MYSQLI_NUM);
+	}
+	else
+		echo 'not img';
+	var_dump($_FILES);
+?>
