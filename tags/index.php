da4aeabee63b46e7764235e4a4ff1b44cef22a48
BourgondAries <macocio@gmail.com>
2015-08-21 11:03:09 +0200

    Add tag counting to the tag page

--- tags/index.php
+++ tags/index.php
@@ -1,10 +1,21 @@
 <?php
 	$root = $_SERVER['DOCUMENT_ROOT'];
 	require_once "$root/template/Template.php";
+	require_once "$root/utils/E621.php";
 	// Get tag data from the database, count the most popular tags.
+	$db = new E621;
+	$con = $db->get();
+	$prep = $con->prepare('SELECT description, COUNT(MediaTag.tag_ID) FROM MediaTag JOIN Tag ON MediaTag.tag_ID = Tag.tag_ID GROUP BY MediaTag.tag_ID;');
+	$prep->execute();
+	$res = $prep->get_result();
+	$tags = [];
+	while ($result = $res->fetch_array())
+	{
+		$tags[] = $result[0];
+		$tags[] = $result[1];
+	}
 
 	// Algorithm for creating the text:
-	$tags = ['class', 293913, 'modulo', 253924, 'something', 230194, 'diamond', 183123];
 	$all = '';
 	$length = count($tags);
 	for ($index = 0; $index < $length; $index += 2)
