1954cb6a018b0afa878176fc70f89d3ad66cfbcf
BourgondAries <macocio@gmail.com>
2015-08-21 13:06:55 +0200

    Return a correct result set for the search.

--- search/search.php
+++ search/search.php
@@ -1,6 +1,8 @@
 <?php
 	$root = $_SERVER['DOCUMENT_ROOT'];
 	require_once "$root/utils/E621.php";
+	require_once "$root/template/Template.php";
+	require_once "$root/result/Result.php";
 	$db = new E621;
 	$dbc = $db->get();
 
@@ -13,16 +15,34 @@
 	$tag_ids = [];
 	while ($fetched = $prep->fetch_array())
 		$tag_ids[] = $fetched[0];
+	if (empty($tag_ids))
+		die();
 	$fullquery = implode(', ', $tag_ids);
 	$tagcount = count($tag_ids);
-	$template = "SELECT media_ID FROM MediaTag WHERE tag_ID IN ($fullquery) GROUP BY media_ID HAVING count(distinct tag_ID) >= $tagcount;";
+	$template = "SELECT media_ID, filename FROM MediaTag NATURAL JOIN Media WHERE tag_ID IN ($fullquery) GROUP BY media_ID HAVING count(distinct tag_ID) >= $tagcount ORDER BY upload_date DESC LIMIT 50;";
 	$prep = $dbc->query($template);
-	echo $dbc->error;
-	echo '<br>' . $template . '<br>';
-	while ($fetched = $prep->fetch_object())
+	$item = [];
+	while ($fetched = $prep->fetch_array())
 	{
-		var_dump($fetched);
-		echo '<br>';
-		echo 'ayy';
+		$infoget = $dbc->prepare("SELECT sum(vote = true) as ups, sum(vote = false) as downs, sum(favorite) as favs FROM UserFeedback WHERE media_ID = ?;");
+		$id = $fetched[0];
+		$infoget->bind_param('i', $id);
+		$infoget->execute();
+		$final_info = $infoget->get_result();
+		$partial_result = $final_info->fetch_array(MYSQLI_NUM);
+		for ($i = 0; $i < 3; ++$i)
+			$partial_result[$i] = $partial_result[$i] == null ? 0 : $partial_result[$i];
+		array_push($item, array
+		(
+			'id' => $fetched[0],
+			'up' => $partial_result[0],
+			'fav' => $partial_result[2],
+			'down' => $partial_result[1],
+			'type' => 'NSFW',
+
+			'thumb' => $fetched[1]
+		));
 	}
+	$result = generateResult($item);
+	echo generateTemplate($result);
 ?>
