9b2777e5dbd2e809c77d842ed3b65a5bc7ad712a
BourgondAries <macocio@gmail.com>
2015-08-22 01:19:50 +0200

    Add prototype css and html template

---
+++ newer/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style> @import url("/newer/reset.css"); </style>
+		<style> @import url("/newer/index.css"); </style>
+	</head>
+	<body>
+		<div class="generic">
+			<div class="row">
+				<div class="prefoot">
+					<div class="table">
+						<div class="row">
+							<div class="navigation_bar">
+								<div class="table">
+									<a href="#"> e622 </a>
+									<a href="#"> LOGIN </a>
+									<a href="#"> UPLOAD </a>
+									<a href="#"> TAGS </a>
+									<a href="#"> SUPPORT </a>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="content">
+								<p>1010</p>
+								<p>1010</p>
+								<p>1010</p>
+								<p>1010</p>
+								<p>1010</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="footer">
+					<div class="table">
+						This is a footer!
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
