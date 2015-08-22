07d10e57b7add7abade4079c4c38be6740ab78d8
BourgondAries <macocio@gmail.com>
2015-08-22 05:45:01 +0200

    Move the html into a renderer

---
+++ newer/views/Standard.php
@@ -0,0 +1,104 @@
+<?php
+	class Standard
+	{
+		function render($main_content, $under_search_bar)
+		{
+			$inserter = [$under_search_bar, $main_content];
+			$generated = '';
+			foreach ($this->template as &$part)
+			{
+				$generated .= $part . array_pop($inserter);
+			}
+			return $generated;
+		}
+
+		private $template =
+		[
+			'<!DOCTYPE html>
+			<html>
+				<head>
+					<style> @import url("/newer/reset.css"); </style>
+					<style> @import url("/newer/index.css"); </style>
+				</head>
+				<body>
+					<div class="generic">
+						<div class="row">
+							<div class="prefoot">
+								<div class="table">
+									<div class="row">
+										<div class="navigation_bar">
+											<div class="table">
+												<a href="#"> e622 </a>
+												<a href="#"> LOGIN </a>
+												<a href="#"> UPLOAD </a>
+												<a href="#"> TAGS </a>
+												<a href="#"> SUPPORT </a>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="content">
+											<div class="table">
+												<div class="cell-right-marged">
+													<div class="searchtable">
+														<div class="row">
+															Search <a class="searchhelp" href="/help">(search help)</a>
+														</div>
+														<div class="row">
+															<form action="/search/search.php" method="get">
+																<input class="searchbar" name="tags" placeholder="your tags" type="text">
+																<br>
+																<input class="searchbar_button" name="submit" type="submit" value="search">
+															</form>
+														</div>
+														<div class="row">',
+
+														'</div>
+													</div>
+												</div>
+												<div class="cell">',
+
+												'</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="footer">
+								<div class="center">
+									<div class="autotable">
+										<div class="row">
+											<div class="cell">',
+
+												'Design
+											</div>
+											<div class="cell">
+												|
+											</div>
+											<div class="cell">
+												Kevin R. S.
+											</div>
+										</div>
+										<div class="row">
+											<div class="cell">
+												Website
+											</div>
+											<div class="cell">
+												|
+											</div>
+											<div class="cell">
+												e622
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</body>
+			</html>'
+		];
+	}
+?>
