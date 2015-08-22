95dede804da1e4b822aa8ab8e9f5136c3bee3a62
BourgondAries <macocio@gmail.com>
2015-08-22 02:50:09 +0200

    Make the bg darker

--- newer/index.php
+++ newer/index.php
@@ -24,43 +24,6 @@
 							<div class="content">
 								<p>Here we put the content.</p>
 								<p>Some lyrics:</p>
-								<p>
-									All around me are familiar faces<br>
-									Worn out places, worn out faces<br>
-									Bright and early for the daily races<br>
-									Going nowhere, going nowhere<br>
-<br>
-									Their tears are filling up their glasses<br>
-									No expression, no expression<br>
-									Hide my head I wanna drown my sorrow<br>
-									No tomorrow, no tomorrow<br>
-<br>
-									And I find it kind of funny<br>
-									I find it kind of sad<br>
-									The dreams in which I'm dying are the best I've ever had<br>
-									I find it hard to tell you,<br>
-									I find it hard to take<br>
-									When people run in circles it's a very, very<br>
-									Mad world, mad world<br>
-<br>
-									Children waiting for the day they feel good<br>
-									Happy birthday, happy birthday<br>
-									And to feel the way that every child should<br>
-									Sit and listen, sit and listen<br>
-<br>
-									Went to school and I was very nervous<br>
-									No one knew me, no one knew me<br>
-									Hello teacher tell me, what's my lesson?<br>
-									Look right through me, look right through me<br>
-<br>
-									And I find it kind of funny<br>
-									I find it kind of sad<br>
-									The dreams in which I'm dying are the best I've ever had<br>
-									I find it hard to tell you,<br>
-									I find it hard to take<br>
-									When people run in circles it's a very, very<br>
-									Mad world, mad world, enlarging your world, mad world<br>
-								</p>
 							</div>
 						</div>
 					</div>
