36e6a7097e4250fd3222aecc3c54ea7d76ce603f
BourgondAries <macocio@gmail.com>
2015-08-24 00:07:55 +0200

    Add controller userpage check

--- controllers/user.php
+++ controllers/user.php
@@ -2,7 +2,11 @@
 	require_once 'models/Http.php';
 	require_once 'models/User.php';
 	require_once 'views/Standard.php';
+	require_once 'views/Userpage.php';
 	$username = Http::get('name');
+	$logged_in_as = User::getCurrentLogin();
+	if ($username == $logged_in_as)
+		;
 
-	echo Standard::render('Username', 'Wow!', User::generateLoginState());
+	echo Standard::render('Username', Userpage::renderPrivileged($username, 'd'), User::generateLoginState());
 ?>
