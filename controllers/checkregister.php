06360be6bd66319d294002f83370123c033508a6
BourgondAries <macocio@gmail.com>
2015-08-23 05:26:15 +0200

    Add password registration

--- controllers/checkregister.php
+++ controllers/checkregister.php
@@ -1,14 +1,15 @@
 <?php
 	require_once 'models/Http.php';
-	require_once 'models/PasswordHash.php';
 	require_once 'models/User.php';
 
-	$user = new User;
 	if (Http::has('password') != null && Http::has('password_retype') != null)
 	{
 
 	}
-	if (Http::get('password') != Http::get('password_retype')
+	if (Http::get('password') != Http::get('password_retype'))
 	{
 	}
+
+	$user = new User;
+	echo $user->register(Http::get('username'), Http::get('email'), Http::get('password'));
 ?>
--- controllers/models/User.php
+++ controllers/models/User.php
@@ -1,12 +1,18 @@
 <?php
+	require_once 'pwhash/PasswordHash.php';
 	class User
 	{
-		function register($username, $email, $pwhash)
+		function register($username, $email, $password)
 		{
 			// Check if the email is valid
 			if (!filter_var($email, FILTER_VALIDATE_EMAIL))
 				return 'INVALID_EMAIL';
 			// Check if uername is in the db already.
+			$password = file_get_contents("${_SERVER['DOCUMENT_ROOT']}/../mysqlkey.txt");
+			$db = new mysqli('127.0.0.1', 'server', $password, 'e622', 3306);
+			if ($db->connect_errno)
+				return 'Failed to connect to database';
+			$db->query('SELECT * FROM User;');
 			// Check if the email is already used.
 
 			// Insert it all into the database
