854f8dafd9dc773371e1c86296b30e6b5ef37c68
BourgondAries <macocio@gmail.com>
2015-08-23 21:43:30 +0200

    Add database getting

--- controllers/models/User.php
+++ controllers/models/User.php
@@ -1,4 +1,5 @@
 <?php
+	require_once 'db/Database.php';
 	require_once 'pwhash/PasswordHash.php';
 	class User
 	{
@@ -6,13 +7,23 @@
 		{
 			// Check if the email is valid
 			if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-				return 'INVALID_EMAIL';
-			// Check if uername is in the db already.
-			$password = file_get_contents("${_SERVER['DOCUMENT_ROOT']}/../mysqlkey.txt");
-			$db = new mysqli('127.0.0.1', 'server', $password, 'e622', 3306);
-			if ($db->connect_errno)
-				return 'Failed to connect to database';
-			$db->query('SELECT * FROM User;');
+				return 'invalid_email';
+
+			// Check if the username is special
+			if ($username == 'root')
+				return 'username_special';
+
+			// Check if username is in the db already.
+			$dbc = new Database;
+			$db = $dbc->get();
+			$result = $db->query('SELECT * FROM User;');
+			while ($row = $result->fetch_assoc())
+			{
+				var_dump($row);
+				echo '<br>';
+				echo '<br>';
+			}
+
 			// Check if the email is already used.
 
 			// Insert it all into the database
