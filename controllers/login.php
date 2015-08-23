b47071fd51379093cc18977f4f1e5ce3355d71a2
BourgondAries <macocio@gmail.com>
2015-08-23 23:18:21 +0200

    Add login reason values

--- controllers/login.php
+++ controllers/login.php
@@ -3,6 +3,18 @@
 	require_once 'views/Standard.php';
 	require_once 'views/Login.php';
 	$reason = Http::get('reason');
+	switch ($reason)
+	{
+		case 'wrong_password':
+			$reason = 'The password you entered was wrong, please try again.';
+		break;
+		case 'user_does_not_exist':
+			$reason = 'No user exists by that username, maybe you want to register a new user?';
+		break;
+		default:
+			$reason = 'An error occurred, please contact support.';
+		break;
+	}
 	$result = Login::renderReason($reason);
 	echo Standard::render($result, Login::render());
 ?>
