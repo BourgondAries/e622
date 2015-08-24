9874a8be95cd93ab79930cce51978d4f06a3cf63
BourgondAries <macocio@gmail.com>
2015-08-24 03:37:00 +0200

    Update style and add renderer for the user page

--- controllers/views/Userpage.php
+++ controllers/views/Userpage.php
@@ -1,6 +1,33 @@
 <?php
 	class Userpage
 	{
+		static function renderNotExist($username)
+		{
+			return self::$code_not_exist[0] . $username . self::$code_not_exist[1];
+		}
+
+		static private $code_not_exist =
+		[
+			'<div class="warning"> The user "',
+
+			'" does not exist.</div>'
+		];
+
+		static function renderPrivilegedStatistics()
+		{
+			return self::$statistics[0];
+		}
+
+		static private $statistics =
+		[
+			'<div class="smalltext"> Here you can change your settings
+			and see some statistics. </div>
+			<div class="space vertical"></div>
+			<form action="/logout" method="post">
+				<input type="submit" value="log out">
+			</form>'
+		];
+
 		static function renderPrivileged($username, $email)
 		{
 			return self::$code[0] . $username . self::$code[1] . $email . self::$code[2];
@@ -8,16 +35,23 @@
 
 		static private $code =
 		[
-			'<div class="box">
-				<div class="boxpad">
-					<div class="row">
+			' <div class="row">
 						USER PAGE
 					</div>
 					<form action="/changeuser" method="post">
-						<div class="table">
+						<div class="autotable">
+							<div class="atablecol">
+							</div>
+							<div class="atablecol">
+							</div>
+							<div class="tablecol">
+							</div>
 							<div class="row">
 								<div class="cell">
 									Username
+								</div>
+								<div class="cell">
+									<div class="space horizontal"></div>
 								</div>
 								<div class="cell">
 									<input class="searchbar" name="username" placeholder="',
@@ -30,16 +64,40 @@
 									Email
 								</div>
 								<div class="cell">
+									<div class="space horizontal"></div>
+								</div>
+								<div class="cell">
 									<input class="searchbar" name="email" placeholder="',
 
 									'" type="text">
 								</div>
 							</div>
+							<div class="row">
+								<div class="cell">
+									Password
+								</div>
+								<div class="cell">
+									<div class="space horizontal"></div>
+								</div>
+								<div class="cell">
+									<input name="password" type="password">
+								</div>
+							</div>
+							<div class="row">
+								<div class="cell">
+									Re-type password
+								</div>
+								<div class="cell">
+									<div class="space horizontal"></div>
+								</div>
+								<div class="cell">
+									<input name="password_retype" type="password">
+								</div>
+							</div>
 						</div>
-						<input type="submit" value="log in">
-					</form>
-				</div>
-			</div>'
+						<div class="space vertical"></div>
+						<input type="submit" value="update">
+					</form>'
 		];
 	}
 ?>
