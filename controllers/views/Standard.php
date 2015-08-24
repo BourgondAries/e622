6f0569522054659e1670e1572327aebde53df274
BourgondAries <macocio@gmail.com>
2015-08-24 05:55:29 +0200

    Simplify CSS even more

--- controllers/views/Standard.php
+++ controllers/views/Standard.php
@@ -21,84 +21,81 @@
 					<style> @import url("/controllers/views/style/index.css"); </style>
 				</head>
 				<body class="e622">
-					<div class="generic">
-						<div class="row">
-							<div class="prefoot">
-								<div class="table">
-									<div class="row">
-										<div class="navigation_bar">
-											<div class="table">
-												<a href="/"> e622 </a>
-												<a href="',
+					<div class="row">
+						<div class="prefoot">
+							<div class="table">
+								<div class="row">
+									<div class="navigation_bar">
+										<div class="table">
+											<a href="/"> e622 </a>
+											<a href="',
 
-												'">',
+											'">',
 
-												'</a>
-												<a href="#"> UPLOAD </a>
-												<a href="#"> TAGS </a>
-												<a href="#"> SUPPORT </a>
-											</div>
+											'</a>
+											<a href="#"> UPLOAD </a>
+											<a href="#"> TAGS </a>
+											<a href="#"> SUPPORT </a>
 										</div>
 									</div>
-									<div class="row">
-										<div class="content">
-											<div class="table">
-												<div class="small table-column"></div>
-												<div class="tiny table-column"></div>
-												<div class="max table-column"></div>
-												<div class="cell">
-													<div class="searchtable">
-														<div class="row">
-															Search <a class="tinytext" href="/help">(search help)</a>
-														</div>
-														<div class="row">
-															<form action="/search/search.php" method="get">
-																<input class="searchbar" name="tags" placeholder="your tags" type="text">
-																<br>
-																<input class="searchbar_button" name="submit" type="submit" value="search">
-															</form>
-														</div>
-														<div class="row">',
-
-														'</div>
+								</div>
+								<div class="row">
+									<div class="content">
+										<div class="table">
+											<div class="small table-column"></div>
+											<div class="tiny table-column"></div>
+											<div class="max table-column"></div>
+											<div class="cell">
+												<div class="table">
+													<div class="row">
+														Search <a class="tinytext" href="/help">(search help)</a>
 													</div>
-												</div>
-												<div class="cell"></div>
-												<div class="cell">',
+													<div class="row">
+														<form action="/search/search.php" method="get">
+															<input name="tags" placeholder="your tags" type="text">
+															<input name="submit" type="submit" value="search">
+														</form>
+													</div>
+													<div class="row">',
 
-												'</div>
+													'</div>
+												</div>
 											</div>
+											<div class="cell"></div>
+											<div class="cell">',
+
+											'</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div class="row">
-							<div class="footer">
-								<div class="center">
-									<div class="autotable">
-										<div class="row">
-											<div class="cell">',
+					</div>
+					<div class="row">
+						<div class="footer">
+							<div class="center">
+								<div class="autotable">
+									<div class="row">
+										<div class="cell">',
 
-												'Design
-											</div>
-											<div class="cell">
-												|
-											</div>
-											<div class="cell">
-												Kevin R. S.
-											</div>
+											'Design
 										</div>
-										<div class="row">
-											<div class="cell">
-												Website
-											</div>
-											<div class="cell">
-												|
-											</div>
-											<div class="cell">
-												e622
-											</div>
+										<div class="cell">
+											|
+										</div>
+										<div class="cell">
+											Kevin R. S.
+										</div>
+									</div>
+									<div class="row">
+										<div class="cell">
+											Website
+										</div>
+										<div class="cell">
+											|
+										</div>
+										<div class="cell">
+											e622
 										</div>
 									</div>
 								</div>
