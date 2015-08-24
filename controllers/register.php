017d27f3339d8f5ccabc3920d9e7ad1a56c11cf4
BourgondAries <macocio@gmail.com>
2015-08-24 07:51:41 +0200

    Generate a sidebar for the register command

--- controllers/register.php
+++ controllers/register.php
@@ -1,8 +1,11 @@
 <?php
+	require_once 'models/Http.php';
 	require_once 'models/User.php';
 	require_once 'views/Standard.php';
 	require_once 'views/Register.php';
 
 	$register_form = Register::render();
-	echo Standard::render('', $register_form, User::generateLoginState());
+	$reason = Register::renderReason(Http::get('reason'));
+	$information = Register::renderInformation();
+	echo Standard::render($information . $reason, $register_form, User::generateLoginState());
 ?>
