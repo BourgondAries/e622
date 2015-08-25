3e94278f451ff802481a8a21be66305e65d0d619
BourgondAries <macocio@gmail.com>
2015-08-25 09:32:41 +0200

    Finish most of the uploading code

--- controllers/models/Upload.php
+++ controllers/models/Upload.php
@@ -36,6 +36,48 @@
 			}
 		}
 
+		private function insertMedia($uploaderid, $description, $file, $extension)
+		{
+			$db = $this->dbc->get();
+			if ($prepare = $db->prepare('INSERT INTO Media (description,  uploader) VALUES (?, ?);'))
+			{
+				$prepare->bind_param('si', $description, $uploaderid);
+				$prepare->execute();
+				$mediaid = $db->insert_id;
+				if ($prepare = $db->prepare('UPDATE Media SET filename = ? WHERE media_ID = ?;'))
+				{
+					$newname = base_convert($mediaid, 10, 36) . ".$extension";
+					$prepare->bind_param('si', $newname, $mediaid);
+					$prepare->execute();
+					$root = $_SERVER['DOCUMENT_ROOT'];
+					rename($file, "$root/storage/" . $newname);
+				}
+			}
+		}
+
+		static function startsWith($string, $query)
+		{
+			return substr($string, 0, strlen($query)) === $query;
+		}
+
+		function getFileType($file)
+		{
+			$finfo = finfo_open(FILEINFO_MIME_TYPE);
+			$type = finfo_file($finfo, $file);
+			finfo_close($finfo);
+			if (!self::startsWith($type, 'image') && !self::startsWith($type, 'video'))
+				return false;
+			switch ($type)
+			{
+				case 'image/gif': return 'gif';
+				case 'image/png': return 'png';
+				case 'image/jpg': return 'jpg';
+				case 'image/jpeg': return 'jpeg';
+				case 'video/webm': return 'webm';
+				default: return false;
+			}
+		}
+
 		function uploadFile($uploader, $description, $tags, $file)
 		{
 			// Check if the uploader has enough privileges
@@ -56,15 +98,18 @@
 			}
 			$unsorted_tags = array_slice($tags, 0, $counter);
 			$sorted_tags = array_slice($tags, $counter + 1);
-			var_dump($unsorted_tags);
 			sort($sorted_tags, SORT_STRING);
-			var_dump($sorted_tags);
 
 			$tag_ids = [];
 			foreach ($unsorted_tags as &$tag)
 				$tag_ids[] = $this->insertTag($tag);
 			foreach ($sorted_tags as &$tag)
 				$tag_ids[] = $this->insertTag($tag);
+			var_dump($tag_ids);
+			$extension = $this->getFileType($file);
+			if ($extension == false)
+				return false;
+			$this->insertMedia($uploader['user_ID'], $description, $file, $extension);
 		}
 	}
 ?>
