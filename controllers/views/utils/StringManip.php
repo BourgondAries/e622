2e55480faa67fdeb1eabaefe776acf4086ed4765
BourgondAries <macocio@gmail.com>
2015-08-28 09:46:36 +0200

    Fix the indentation of the intermix function

--- controllers/views/utils/StringManip.php
+++ controllers/views/utils/StringManip.php
@@ -1,13 +1,13 @@
 <?php
-		function intermix($template, $array)
+	function intermix($template, $array)
+	{
+		$result = '';
+		for ($i = 0; $i < count($template); ++$i)
 		{
-			$result = '';
-			for ($i = 0; $i < count($template); ++$i)
-			{
-				$result .= $template[$i];
-				if ($i < count($array))
-					$result .= $array[$i];
-			}
-			return $result;
+			$result .= $template[$i];
+			if ($i < count($array))
+				$result .= $array[$i];
 		}
+		return $result;
+	}
 ?>
