b49c0751e6dc48fa613c1b77743d9e214c7bf39e
BourgondAries <macocio@gmail.com>
2015-08-28 15:09:00 +0200

    Put the max height at 90vh and implement webm support

--- controllers/views/MediaPage.php
+++ controllers/views/MediaPage.php
@@ -4,8 +4,20 @@
 	{
 		static function render($media_data)
 		{
-			return intermix(self::$code, [$media_data['filename']]);
+			if (getExtension($media_data['filename']) == 'webm')
+				return intermix(self::$video_code, [$media_data['filename']]);
+			else
+				return intermix(self::$code, [$media_data['filename']]);
 		}
+
+		static private $video_code =
+		[
+			'<video class="media" controls>
+				<source src="/storage/',
+
+				'" type="video/webm">
+			</video>'
+		];
 
 		static private $code =
 		[
